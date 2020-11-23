61aeecfb6fac1bb7f1bc1a70f5589abf0802c244
Dustin Bailey <bailey.dust@gmail.com>
2020-11-23 12:42:11 -0800

    Cocoa demo running removing token from sentry-cocoa.xcodeproj

--- sentry-cocoa/AppDelegate.swift
+++ sentry-cocoa/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         SentrySDK.start { options in
-            options.dsn = "https://be3a9d11afe54cfea9aa0ced1c18a4e2@o87286.ingest.sentry.io/5316623"
+            options.dsn = "https://cdc559058f7440889c5eb33c55dbbcf3@o87286.ingest.sentry.io/5529240"
             options.debug = true
             options.environment = "Production"
             options.enableAutoSessionTracking = true
